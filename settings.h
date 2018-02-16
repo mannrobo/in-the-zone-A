2eb56301899dd284c54cb2df299d5bdb6aa82844
Ben Godfrey <BenJetson@users.noreply.github.com>
2018-02-16 11:44:32 -0500

    Fix drive axis defaults

--- settings.h
+++ settings.h
@@ -2,7 +2,7 @@
 #ifndef ROBOT_SETTINGS
 #define ROBOT_SETTINGS
 
-const bool INV_DRV_A = false; //DEFAULT: false
+const bool INV_DRV_A = true; //DEFAULT: true
 const bool INV_DRV_B = true; //DEFAULT: true
 
 const bool KS_DEFAULT = false;  //DEFAULTS: { true  for practice
